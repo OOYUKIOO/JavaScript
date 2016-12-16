af0462882355ba71399b404c8a0d2f9471e3af94
JiaqiGao <jiaqigao12@gmail.com>
2016-12-15 20:11:39 -0500

    completed first assignment

--- intro.js
+++ intro.js
@@ -2,7 +2,24 @@
 
 //Change text of h1
 var items = document.getElementsByTagName('li');
-console.log(items);
+
+var exclaim = 0;
+
+//Upon button push, add an element to the list.
+var addTolist = function(){
+    var li = document.createElement("li");
+    var input = "Tadaa";
+    var exclaims = "";
+    for (var i = 0; i < exclaim; i++){
+	exclaims += "!";
+    }
+    li.innerHTML = input+exclaims;
+    document.getElementById("thelist").appendChild(li);
+    exclaim += 1;
+}
+
+
+
 for (var i = 0; i < items.length; i++){
     items[i].addEventListener("mouseover",function(e){
 	document.getElementById("h").innerHTML = this.innerHTML;
