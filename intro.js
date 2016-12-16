af2f1a230d850f4618f01b71494e420525603cbc
OOYUKIOO <yuxuan.chen.up@gmail.com>
2016-12-15 19:39:43 -0500

    finished changing h1 text javascript

--- intro.js
+++ intro.js
@@ -1,10 +1,14 @@
 //JS CODE
 
+//Change text of h1
 var items = document.getElementsByTagName('li');
 console.log(items);
-for(var i = 0; i < items.length; i++){
-    items[i].addEventListener('mouseover',changeTitle);
-};
-var changeTitle = function(){
+for (var i = 0; i < items.length; i++){
+    items[i].addEventListener("mouseover",function(e){
+	document.getElementById("h").innerHTML = this.innerHTML;
+    },false);
 
-};
+    items[i].addEventListener("mouseout",function(e){
+	document.getElementById("h").innerHTML = "Hello World!";
+    },false);
+}
